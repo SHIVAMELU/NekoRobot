73f7461eddc5ffe4542642f877e86542afe322a7
SHIVAMELU <87264996+SHIVAMELU@users.noreply.github.com>
2023-04-24 10:27:51 +0530

    Update config.py

--- NekoRobot/config.py
+++ NekoRobot/config.py
@@ -30,7 +30,9 @@ class Config(object):
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = "postgres://vbvxzart:STzbiVxGC4_iOhbcJR7KT0MMsW1Y0umT@balarama.db.elephantsql.com/vbvxzart"  # needed for any database modules
-    DB_URL = "aor5rcv.mongodb.net/?retryWrites=true&w=majority"
+    DB_URL = "mongodb+srv://eren:eren@cluster0.aor5rcv.mongodb.net/?retryWrites=true&w=majority"
+
+"
 
 "
     REDIS_URL = "redis-cli -u redis://default:2bOod2HwLNZdLvgQZKPYGn7DCsJsr3rF@redis-13503.c56.east-us.azure.cloud.redislabs.com:13503"
