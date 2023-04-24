fc04bf032ba282ef2eb3f52345ddd254f4b3fd6a
SHIVAMELU <87264996+SHIVAMELU@users.noreply.github.com>
2023-04-24 10:42:25 +0530

    Update config.py

--- NekoRobot/config.py
+++ NekoRobot/config.py
@@ -42,9 +42,9 @@ class Config(object):
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = ("5715764478", "sudos")
+    DRAGONS = ("", "sudos")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("5348193047", "devs")
+    DEV_USERS = get_user_list("", "devs")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     DEMONS = get_user_list("elevated_users.json", "supports")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
